c06ac825600aed33e2cae89d15b15f50a31ad6c8
iamr0b0x <abduladeneye@gmail.com>
2022-06-20 19:26:46 +0100

    url changes

--- flipbook/tests/urls.py
+++ flipbook/tests/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path
 
 admin.autodiscover()
 
@@ -12,7 +12,7 @@ urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
 
 urlpatterns += [
-    path(r'admin/', admin.site.urls),
-    path(r'flipbooks/', include('flipbook.urls')),
-    path(r'summernote/', include('django_summernote.urls')),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^flipbooks/', include('flipbook.urls')),
+    re_path(r'^summernote/', include('django_summernote.urls')),
 ]
