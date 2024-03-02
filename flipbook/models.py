6265bc97731c405ad7edbdf5fca01b455930d81f
Tobias Lorenz <tobias.lorenz@rtl.de>
2024-03-02 22:10:59 +0100

    feat: added password field

--- flipbook/models.py
+++ flipbook/models.py
@@ -1,4 +1,5 @@
 """Models for the flipbook app."""
+
 from django.conf import settings
 from django.urls import reverse
 from django.db import models
@@ -21,47 +22,51 @@ class FlipbookCategory(models.Model):
     :email: Optional email address.
 
     """
+
     user = models.ForeignKey(
-        'auth.User',
-        verbose_name=_('User'),
-        related_name='flipbook_categories',
-        blank=True, null=True,
+        "auth.User",
+        verbose_name=_("User"),
+        related_name="flipbook_categories",
+        blank=True,
+        null=True,
         on_delete=models.CASCADE,
     )
 
     title = models.CharField(
         max_length=100,
-        verbose_name=_('Title'),
+        verbose_name=_("Title"),
     )
 
     slug = models.SlugField(
         max_length=100,
-        verbose_name=_('Slug'),
+        verbose_name=_("Slug"),
         unique=True,
     )
 
     small_image = FilerImageField(
-        verbose_name=_('Small image'),
-        blank=True, null=True,
-        related_name='flipbook_categories_with_small_images',
+        verbose_name=_("Small image"),
+        blank=True,
+        null=True,
+        related_name="flipbook_categories_with_small_images",
         on_delete=models.CASCADE,
     )
 
     large_image = FilerImageField(
-        verbose_name=_('Large image'),
-        blank=True, null=True,
-        related_name='flipbook_categories_with_large_images',
+        verbose_name=_("Large image"),
+        blank=True,
+        null=True,
+        related_name="flipbook_categories_with_large_images",
         on_delete=models.CASCADE,
     )
 
     url = models.URLField(
-        verbose_name=_('URL'),
+        verbose_name=_("URL"),
         max_length=200,
         blank=True,
     )
 
     email = models.EmailField(
-        verbose_name=_('eMail'),
+        verbose_name=_("eMail"),
         max_length=200,
         blank=True,
     )
@@ -70,7 +75,7 @@ class FlipbookCategory(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('flipbook_category', kwargs={'slug': self.slug})
+        return reverse("flipbook_category", kwargs={"slug": self.slug})
 
 
 class Flipbook(models.Model):
@@ -87,84 +92,95 @@ class Flipbook(models.Model):
     :book_type: Optional book type (e.g. to distinguish formats).
     :url: Optional external url.
     :email: Optional email address.
+    :password: Password to hide the book for unwanted users.
 
     """
+
     user = models.ForeignKey(
-        'auth.User',
-        verbose_name=_('User'),
-        related_name='flipbooks',
-        blank=True, null=True,
+        "auth.User",
+        verbose_name=_("User"),
+        related_name="flipbooks",
+        blank=True,
+        null=True,
         on_delete=models.CASCADE,
     )
 
     title = models.CharField(
         max_length=100,
-        verbose_name=_('Title'),
+        verbose_name=_("Title"),
     )
 
     category = models.ForeignKey(
         FlipbookCategory,
-        verbose_name=_('Category'),
-        blank=True, null=True,
-        related_name='flipbooks',
+        verbose_name=_("Category"),
+        blank=True,
+        null=True,
+        related_name="flipbooks",
         on_delete=models.CASCADE,
     )
 
     slug = models.SlugField(
         max_length=100,
-        verbose_name=_('Slug'),
+        verbose_name=_("Slug"),
         unique=True,
     )
 
     is_published = models.BooleanField(
-        verbose_name=_('Is published'),
+        verbose_name=_("Is published"),
         default=False,
     )
 
     download = FilerFileField(
-        verbose_name=_('Download'),
-        blank=True, null=True,
-        related_name='download_flipbooks',
+        verbose_name=_("Download"),
+        blank=True,
+        null=True,
+        related_name="download_flipbooks",
         on_delete=models.CASCADE,
     )
 
     download_link = models.URLField(
-        verbose_name=_('Download Link'),
+        verbose_name=_("Download Link"),
         blank=True,
     )
 
     video_download = FilerFileField(
-        verbose_name=_('Video Download'),
-        blank=True, null=True,
-        related_name='video_download_flipbooks',
+        verbose_name=_("Video Download"),
+        blank=True,
+        null=True,
+        related_name="video_download_flipbooks",
         on_delete=models.CASCADE,
     )
 
     image = FilerImageField(
-        verbose_name=_('Image'),
-        blank=True, null=True,
-        related_name='flipbooks',
+        verbose_name=_("Image"),
+        blank=True,
+        null=True,
+        related_name="flipbooks",
         on_delete=models.CASCADE,
     )
 
     book_type = models.CharField(
         max_length=256,
-        verbose_name=_('Type'),
-        choices=getattr(settings, 'FLIPBOOK_BOOK_TYPE', (
-            ('default', _('default')), )
-        ),
+        verbose_name=_("Type"),
+        choices=getattr(settings, "FLIPBOOK_BOOK_TYPE", (("default", _("default")),)),
         blank=True,
     )
 
     url = models.URLField(
-        verbose_name=_('URL'),
+        verbose_name=_("URL"),
         max_length=200,
         blank=True,
     )
 
     email = models.EmailField(
-        verbose_name=_('eMail'),
+        verbose_name=_("eMail"),
         max_length=200,
+        blank=True,
+    )
+
+    password = models.CharField(
+        max_length=64,
+        verbose_name=_("Password"),
         blank=True,
     )
 
@@ -175,9 +191,11 @@ class Flipbook(models.Model):
         if self.category:
             category_slug = self.category.slug
         else:
-            category_slug = 'detail'
-        return reverse('flipbook_detail', kwargs={
-            'slug': self.slug, 'category_slug': category_slug})
+            category_slug = "detail"
+        return reverse(
+            "flipbook_detail",
+            kwargs={"slug": self.slug, "category_slug": category_slug},
+        )
 
 
 class FlipbookPage(models.Model):
@@ -193,46 +211,46 @@ class FlipbookPage(models.Model):
       template).
 
     """
+
     flipbook = models.ForeignKey(
         Flipbook,
-        verbose_name=_('Flipbook'),
-        related_name='pages',
+        verbose_name=_("Flipbook"),
+        related_name="pages",
         on_delete=models.CASCADE,
     )
 
     position = models.PositiveIntegerField(
-        verbose_name=_('Position'),
+        verbose_name=_("Position"),
     )
 
     content = models.TextField(
         max_length=8192,
-        verbose_name=_('Content'),
+        verbose_name=_("Content"),
         blank=True,
     )
 
     image = FilerImageField(
-        verbose_name=_('Image'),
-        blank=True, null=True,
-        related_name='pages',
+        verbose_name=_("Image"),
+        blank=True,
+        null=True,
+        related_name="pages",
         on_delete=models.CASCADE,
     )
 
     video_url = models.URLField(
-        verbose_name=_('Video URL'),
+        verbose_name=_("Video URL"),
         blank=True,
     )
 
     page_type = models.CharField(
         max_length=256,
-        verbose_name=_('Type'),
-        choices=getattr(settings, 'FLIPBOOK_PAGE_TYPE', (
-            ('default', _('default')), )
-        ),
+        verbose_name=_("Type"),
+        choices=getattr(settings, "FLIPBOOK_PAGE_TYPE", (("default", _("default")),)),
         blank=True,
     )
 
     class Meta:
-        ordering = ['position', 'flipbook', 'pk']
+        ordering = ["position", "flipbook", "pk"]
 
     def __str__(self):
         return str(self.position)
@@ -248,41 +266,43 @@ class FlipbookDownload(models.Model):
     :file: File.
 
     """
+
     flipbook = models.ForeignKey(
         Flipbook,
-        verbose_name=_('Flipbook'),
-        related_name='downloads',
+        verbose_name=_("Flipbook"),
+        related_name="downloads",
         on_delete=models.CASCADE,
     )
 
     position = models.PositiveIntegerField(
-        verbose_name=_('Position'),
+        verbose_name=_("Position"),
     )
 
     file = FilerFileField(
-        verbose_name=_('File'),
-        blank=True, null=True,
-        related_name='flipbook_downloads',
+        verbose_name=_("File"),
+        blank=True,
+        null=True,
+        related_name="flipbook_downloads",
         on_delete=models.CASCADE,
     )
 
     download_link = models.URLField(
-        verbose_name=_('Download Link'),
+        verbose_name=_("Download Link"),
         blank=True,
     )
 
     file_type = models.CharField(
         max_length=12,
-        verbose_name=_('File type'),
+        verbose_name=_("File type"),
         choices=(
-            ('pdf', _('PDF')),
-            ('video', _('Video')),
+            ("pdf", _("PDF")),
+            ("video", _("Video")),
         ),
         blank=True,
     )
 
     class Meta:
-        ordering = ['position', 'flipbook', 'pk']
+        ordering = ["position", "flipbook", "pk"]
 
     def __str__(self):
         return str(self.position)
